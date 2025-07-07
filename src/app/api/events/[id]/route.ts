8734930ce9261bfe3f784cfda175f29a420e2838
Midhun2783git <99118893+Midhun2783git@users.noreply.github.com>
2025-07-07 20:33:29 +0400

    Update d route.ts

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -1,26 +1,3 @@
-// import { NextRequest, NextResponse } from 'next/server';
-// import prisma from '@/lib/db';
-
-// export async function PATCH(
-//   req: NextRequest,
-//   context: { params: any } // Use 'any' or infer via typeof context.params.id
-// ) {
-//   try {
-//     const { id } = context.params;
-//     const { completed } = await req.json();
-
-//     const updated = await prisma.event.update({
-//       where: { id },
-//       data: { completed },
-//     });
-
-//     return NextResponse.json(updated, { status: 200 });
-//   } catch (error) {
-//     console.error('Error updating event:', error);
-//     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
-//   }
-// }
-
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 
@@ -43,4 +20,3 @@ export async function PATCH(req: NextRequest, context: unknown) {
     return NextResponse.json({ error: 'Failed to update event' }, { status: 500 });
   }
 }
-
