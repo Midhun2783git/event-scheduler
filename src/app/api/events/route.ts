0b61e6af80fec2ad8cc66b25b83c2a4b158d9afd
Midhun2783git <99118893+Midhun2783git@users.noreply.github.com>
2025-07-08 18:07:15 +0400

    Update v1.6

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -30,7 +30,7 @@
 //   }
 // }
 
-import { NextRequest, NextResponse } from 'next/server';
+//import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 
 export async function GET(req: NextRequest) {
